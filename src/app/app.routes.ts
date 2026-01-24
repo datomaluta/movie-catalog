a2afdfa2adcead6780b5203d8bafc9005d0c51ff
datomaluta <datomaluta@gmail.com>
2026-01-24 12:28:31 +0400

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Movies } from './pages/movies/movies';
+import { MovieDetails } from './pages/movie-details/movie-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Movies,
+  },
+  {
+    path: ':id',
+    component: MovieDetails,
+  },
+];
